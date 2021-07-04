a9607dff4878154ad376db880cc274a1358dc6cb
Thomas Todt <thomas.bianchi.todt@gmail.com>
2021-07-04 05:29:36 -0300

    funcao de inclusao, algumas outras funcoes uteis

--- main.c
+++ main.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "BST.h"
+#include "Arvore_A.h"
 
 int main(void)
 {
-    struct tNo *raiz = NULL;
-    int v;
-    char entrada[] = "10 15 5 55 50 7 8 14 13 49\0";
+    struct tNo_A *raiz = NULL;
 
-    raiz = montaarvore(entrada);
+    char comando;
+    char bonsai[100];
+    char continuar = 'y';
 
-    printf("Percurso em ordem: ");
-    emordem(raiz);
-    printf("\n");
-
-    printf("\n");
-    imprime("busca", busca(raiz, 15));
-    imprime("busca", busca(raiz, 50));
-    imprime("busca", busca(raiz, 10));
-    imprime("busca 17", busca(raiz, 17));
-    printf("\n");
-
-    imprime("minimo raiz: ", min(raiz));
-    imprime("minimo 15: ", min(busca(raiz, 15)));
-    imprime("minimo 55: ", min(busca(raiz, 55)));
-    printf("\n");
-
-    imprime("sucessor raiz: ", sucessor(raiz));
-    imprime("sucessor 15: ", sucessor(busca(raiz, 15)));
-    imprime("sucessor 5: ", sucessor(busca(raiz, 5)));
-    imprime("sucessor 8: ", sucessor(busca(raiz, 8)));
-    imprime("sucessor 55: ", sucessor(busca(raiz, 55)));
-    printf("\n");
-
-    v = 49;
-    printf("Exclusão: %d \n", v);
-    raiz = exclui(busca(raiz, v), raiz);
-    emordem(raiz);
+    while(continuar == 'y'){ // na verdade, tem que dar um jeito disso ler um arquivo ate o final
+        scanf("%c", &comando);
+        scanf("%s", bonsai);
+        switch(comando){
+            case 'i':
+                inclui(raiz, bonsai); 
+                break;
+            case 'b':
+                busca(raiz, soma_entrada(bonsai));
+                break;
+            case 'r':
+                exclui(raiz, soma_entrada(bonsai));
+                break;
+        }
+        printf("continuar? (y/n): \n");
+        scanf("%c", continuar); 
+    }
+    /*emordem(raiz);*/
 
     return 0;
 }
